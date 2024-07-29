82ec13d38cd9b594f9b897e48d83e490b3695d06
仧乆 <3130939083@qq.com>
2024-07-29 16:48:30 +0800

    day1

---
+++ applications/main.c
@@ -0,0 +1,16 @@
+#include <rtthread.h>
+// #include <hello.h>
+
+int main(void)
+{
+
+    while(1)
+    {
+        // Print_Hello_World();
+        rt_thread_mdelay(1000);
+    }
+
+    return 0;
+
+}
+
